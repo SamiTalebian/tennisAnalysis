ae57b34e9993086098867294eba51f712ba09d68
SamiTalebian <sami9077@gmail.com>
2022-05-24 10:33:44 +0430

    first commit

---
+++ analysis/admin.py
@@ -0,0 +1,75 @@
+from operator import truediv
+from xmlrpc.client import Boolean
+from django.contrib import admin
+from analysis.models import Payment, PhysicalRecord, Player, Staff, TechnicalRecord
+from django_jalali.admin.filters import JDateFieldListFilter
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name' , 'right_hand', 'dob' , 'height' , 'weight' , 'date_created']
+    search_fields = ['name']
+
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name' , 'date_created', 'time_in_court', 'staff_last_payment','time_in_court_since_last_payment']
+    search_fields = ['name']
+
+
+    def time_in_court(self, obj):
+        sum = 0
+        for i in obj.staffs.all():
+            sum += i.class_duration
+
+        for i in obj.staffs_pr.all():
+            sum += i.class_duration
+        return sum
+    
+    def staff_last_payment(self, obj):
+        return obj.payments.order_by('payment_date').last()
+
+
+    def time_in_court_since_last_payment(self, obj):
+        sum = 0
+        last_payment = obj.payments.order_by('payment_date').last()
+        for i in obj.staffs.all():
+            if str(i.date_created) > str(last_payment):
+                sum += i.class_duration
+        
+        for i in obj.staffs_pr.all():
+            if str(i.date_created) > str(last_payment):
+                sum += i.class_duration
+
+        return sum
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'payment_date', 'staff']
+    list_filter = ['staff']
+
+
+@admin.register(TechnicalRecord)
+class TechnicalRecordAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'player', 'staff' , 'date_created', 'forehand' , 'backhand' , 'serve' , 'volley' , 'movement' , 'listening' , 'has_note']
+    search_fields = ['player__name', 'staff__name']
+
+    list_filter = ['staff']
+
+    def has_note(self, obj) -> Boolean:
+        return len(obj.note) > 0
+
+    has_note.boolean = True
+
+@admin.register(PhysicalRecord)
+class PhysicalRecordAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'player', 'staff' , 'stamina' , 'stretching' , 'movement' , 'attention', 'has_note' ,'date_created']
+    search_fields = ['player__name', 'staff__name']
+
+    list_filter = ['staff']
+
+    def has_note(self, obj) -> Boolean:
+        return len(obj.note) > 0
+
+    has_note.boolean = True
---
+++ analysis/models.py
@@ -0,0 +1,77 @@
+from random import choices
+from sre_constants import RANGE
+from turtle import onscreenclick
+from wsgiref.validate import validator
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django_jalali.db import models as pmodels
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    right_hand = models.BooleanField()
+    dob = pmodels.jDateField()
+    date_created = pmodels.jDateField(auto_now_add=True)
+    height = models.IntegerField(null=True,blank=True)
+    weight = models.IntegerField(null=True,blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Payment(models.Model):
+    payment_date = pmodels.jDateField(null=True, blank=True)
+    staff = models.ForeignKey('Staff', related_name='payments', on_delete=models.DO_NOTHING)
+    amount = models.CharField(max_length=255, null=False, blank=True)
+
+    def __str__(self) -> str:
+        return str(self.payment_date)
+
+
+class Staff(models.Model):
+    STAFF_CHOICES = [
+        ('1', 'Coach'),
+        ('2', 'Trainer'),
+    ]
+
+    name = models.CharField(max_length=255, unique=True)
+    date_created = pmodels.jDateField(auto_now_add=True)
+    staff_type = models.CharField(max_length=2 ,choices=STAFF_CHOICES ,default=1)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+
+class TechnicalRecord(models.Model):
+    RANGE_VALIDATOR = [MaxValueValidator(100), MinValueValidator(0)]
+
+    forehand = models.IntegerField(validators = RANGE_VALIDATOR)
+    backhand = models.IntegerField(validators = RANGE_VALIDATOR)
+    serve = models.IntegerField(validators = RANGE_VALIDATOR)
+    class_duration = models.FloatField(default=1, null=False, blank=False)
+    volley = models.IntegerField(validators = RANGE_VALIDATOR)
+    movement = models.IntegerField(validators = RANGE_VALIDATOR)
+    listening = models.IntegerField(validators = RANGE_VALIDATOR)
+    date_created = pmodels.jDateField(auto_now_add = True)
+    note = models.TextField(null=True, blank=True)
+    player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True)
+    staff = models.ForeignKey(Staff,related_name="staffs" ,on_delete=models.SET_NULL,blank=True, null=True)
+
+
+class PhysicalRecord(models.Model):
+    RANGE_VALIDATOR = [MaxValueValidator(100), MinValueValidator(0)]
+
+    date_created = pmodels.jDateField(auto_now_add = True)
+    stamina = models.IntegerField(validators = RANGE_VALIDATOR)
+    stretching = models.IntegerField(validators= RANGE_VALIDATOR)
+    movement = models.IntegerField(validators= RANGE_VALIDATOR)
+    class_duration = models.FloatField(default=1, null=False, blank=False)
+    attention = models.IntegerField(validators= RANGE_VALIDATOR)
+    note = models.TextField(null=True, blank=True)
+    player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, blank=True)
+    staff = models.ForeignKey(Staff,related_name="staffs_pr" ,on_delete=models.SET_NULL,blank=True, null=True)
+
+
+
+
+
